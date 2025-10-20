10b24cd0eeb211b22b7f8e13b003122b845d4d59
aliaitrahou <alirhou7@gmail.com>
2025-10-20 12:31:07 +0100

    feat(app): initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          '-apple-system',          // San Francisco on macOS & iOS
+          'BlinkMacSystemFont',     // Chrome <-> macOS
+          '"Segoe UI"',             // Windows
+          'Roboto',                 // Android
+          '"Helvetica Neue"',       // Older macOS
+          'Arial',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+};
