2ecf7d9fe1f2f9bee10bc7a26b6e710e371efd27
aliaitrahou <alirhou7@gmail.com>
2025-10-20 20:58:49 +0100

    feat(comps): style footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,28 +4,48 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t border-neutral-300 bg-gray-100 text-neutral-500 text-sm py-4">
-      <div className="max-w-5xl mx-auto px-4 flex flex-col border">
+    <footer className="w-full  bg-gray-50 text-neutral-500 text-sm py-8 border-t border-neutral-200">
+      <div className="max-w-5xl mx-auto flex items-center justify-between pt-2 pb-4 font-light">
 
         {/* Top Link Rows */}
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 text-xs py-4">
-          {/* Left Section — Site Links */}
+        {/* Left Section — Site Links */}
 
-          {/* Right Section — Social Links */}
-          <div className="flex flex-col justify-center sm:justify-end gap-1 sm:gap-2">
-            <Link href="https://instagram.com/thesfhaven" target="_blank" className="hover:text-black transition-colors">
-              Instagram
-            </Link>
-            <Link href="https://x.com/steadfasthaven1" target="_blank" className="hover:text-black transition-colors">
-              X (Twitter)
-            </Link>
-            <Link href="https://facebook.com" target="_blank" className="hover:text-black transition-colors">
-              Facebook
-            </Link>
+        {/* Right Section — Social Links */}
+        <div className="leading-relaxed">
+          <span className="text-black font-semibold">Social Links</span>
+          <div>
+            <p>
+              <a href="https://instagram.com/thesfhaven" target="_blank" className="hover:text-black transition-colors">
+                Instagram
+              </a>
+            </p>
+            <p>
+              <a href="https://x.com/steadfasthaven1" target="_blank" className="hover:text-black transition-colors">
+                X (Twitter)
+              </a>
+            </p>
+
           </div>
+          <div>
+            <p>
+              <a href="https://www.tiktok.com/@steadfasthaven" target="_blank" className="hover:text-black transition-colors">
+                TikTok
+              </a>
+            </p>
+            <p>
+              {/* https://www.youtube.com/@SteadfastHaven */}
+              {/* <a hreLf="https://www.youtube.com/@SteadfastHaven" target="_blank" className="hover:text-black transition-colors" */}
+              {/* Youtube */}
+              {/* </a> */}
+            </p>
+
+          </div>
+        </div>
 
 
-          <div className="text-center sm:text-left leading-relaxed text-xs text-neutral-600">
+        <div className="leading-relaxed">
+          <span className="text-black font-semibold">Get in touch</span>
+          <div>
             <p>
               Email:{' '}
               <a href="mailto:admin@steadfasthaven.com" className="hover:text-black">
@@ -33,24 +53,22 @@ const Footer = () => {
               </a>
             </p>
             <p>
-              Instagram:{' '}
-              <a href="https://instagram.com/thesfhaven" target="_blank" className="hover:text-black">
-                @thesfhaven
-              </a>
+              <Link href="/contact" className="hover:text-black">
+                Contact Us
+              </Link>
             </p>
-          </div>
-
-          <div className="text-center sm:text-left leading-relaxed text-xs text-neutral-600">
-            <p>
-              Youth Program Enrollment:{' '}
-            </p>
-            <span className="text-neutral-400 italic">Coming soon</span>
           </div>
         </div>
 
-
-
+        <div className="text-neutral-600">
+          <span className="text-black font-semibold">Programs</span>
+          <p>
+            Youth Program Enrollment:{' '}
+          </p>
+          <span className="text-neutral-400 italic">Coming soon</span>
+        </div>
       </div>
+
       <div className='max-w-5xl mx-auto pt-4 border-t border-neutral-300 flex flex-row items-center justify-between'>
         <p className="text-center text-xs">
           &copy; {new Date().getFullYear()} Steadfast Haven. All rights reserved.
@@ -62,13 +80,10 @@ const Footer = () => {
           </Link>|
           <Link href="/terms" className="hover:text-black transition-colors">
             Terms of Service
-          </Link>|
-          <Link href="/contact" className="hover:text-black transition-colors">
-            Contact Us
           </Link>
         </div>
         <p className="text-center text-xs">
-          Memphis, TN
+          3932 S Perkins Rd 3932 s, Memphis, TN 38118, USA
         </p>
       </div>
     </footer>
